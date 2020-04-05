fa92446307eb58996521cc61ae81f41e7b064948
hectr <h@mrhector.me>
2020-04-05 20:58:00 +0200

    Implement Endpoint to cURL command mapping

---
+++ Sources/httpClient/endpoints/Endpoint+cURL.swift
@@ -0,0 +1,115 @@
+import Foundation
+import Idioms
+
+extension Endpoint {
+    public func cURL(replaceNewLinesInRawBodyWithSpaces: Bool = false,
+                     replaceTabsInRawBodyWithSpaces: Bool = false,
+                     bodyEncoding: String.Encoding = .utf8) -> String {
+        var command = Self.cURL(endpoint: self,
+                                bodyEncoding: bodyEncoding)
+        if replaceTabsInRawBodyWithSpaces {
+            command = command.replacingOccurrences(of: "\t", with: "    ")
+        }
+        if replaceNewLinesInRawBodyWithSpaces {
+            command = command.replacingOccurrences(of: "\n", with: " ")
+        }
+        return command
+    }
+
+    private static func cURL(endpoint: Endpoint,
+                             bodyEncoding: String.Encoding) -> String {
+        var lines = [String]()
+        lines.append("-X \(endpoint.method.rawValue)")
+        lines.append(contentsOf: buildHeaders(for: endpoint))
+        lines.append(contentsOf: buildBody(for: endpoint, encoding: bodyEncoding))
+        if let url = try? URL.build(with: endpoint) {
+            lines.append(contentsOf: buildCookies(for: url))
+            lines.append("\"\(url.absoluteString)\"")
+        }
+        return lines
+            .map { $0.trimmingCharacters(in: .whitespaces) }
+            .map { $0.prepending("  ") }
+            .prepending("curl")
+            .joined(separator: " \\\n")
+    }
+
+    private static func buildHeaders(for endpoint: Endpoint) -> [String] {
+        endpoint
+            .httpHeaderFields
+            .reduce(into: [String]()) { lines, field in
+                let name = field.key
+                let value = field.value.replacingOccurrences(of: "\'", with: "\\\'")
+                let header = "'\(name): \(value)'"
+                lines.append("-H \(header)")
+                if field.key.lowercased() == "accept-encoding",
+                    value.lowercased().contains(word: "gzip") {
+                    lines.append("--compressed")
+                }
+        }
+    }
+
+    private static func escapedBody(_ string: String) -> String {
+        string
+            .replacingOccurrences(of: "\\", with: "\\\\")
+            .replacingOccurrences(of: "`", with: "\\`")
+            .replacingOccurrences(of: "\"", with: "\\\"")
+            .replacingOccurrences(of: "$", with: "\\$")
+    }
+
+    private static func buildFormParameter(from multipartParameter: MultipartParameter) -> String {
+        var components = [String]()
+        if !multipartParameter.name.isEmpty {
+            components.append("name=\(multipartParameter.name)")
+        }
+        if !multipartParameter.fileName.isEmpty {
+            components.append("filename=\(multipartParameter.fileName)")
+        }
+        components.append("data=@filename")
+        if !multipartParameter.mimeType.isEmpty {
+            components.append("type=\(multipartParameter.mimeType)")
+        }
+        return "--form \(components.joined(separator: ";"))"
+    }
+
+    private static func buildBody(for endpoint: Endpoint, encoding: String.Encoding) -> [String] {
+        switch endpoint.body {
+        case .empty:
+            return []
+
+        case let .string(codableString):
+            return ["--data-binary \"\(escapedBody(codableString.string))\""]
+
+        case let .data(data):
+            if let string = String(data: data, encoding: encoding) {
+                return ["--data-binary \"\(escapedBody(string))\""]
+            } else {
+                return ["--data-binary @filename"]
+            }
+
+        case let .json(bodyParameters):
+            if let string = try? bodyParameters.toJSONString() {
+                return ["--data-binary \"\(escapedBody(string))\""]
+            } else {
+                return ["--data-binary @filename"]
+            }
+
+        case let .formUrlEncoded(parameters):
+            if let string = parameters.toURLComponents().percentEncodedQuery {
+                return ["--data-urlencode \"\(escapedBody(string))\""]
+            } else {
+                return parameters.map { "-d \"\($0.key)=\($0.value)\"" }
+            }
+
+        case let .multipartformData(multipartParameters):
+            return multipartParameters.map { buildFormParameter(from: $0) }
+        }
+    }
+
+    private static func buildCookies(for url: URL) -> [String] {
+        if let cookies = HTTPCookieStorage.shared.cookies(for: url) {
+            return cookies.map { "--cookie \"\($0.name)=\($0.value)\"" }
+        } else {
+            return []
+        }
+    }
+}
---
+++ Tests/httpClientTests/endpoints/EndpointTests.swift
@@ -0,0 +1,96 @@
+import Foundation
+@testable import httpClient
+import XCTest
+
+final class EndpointTests: XCTestCase {
+    func testEmptyBodyCurl() {
+        struct EmptyBody: Endpoint {
+            var baseUrl = "https://example.org"
+            var path = "some/path"
+            var httpHeaderFields: Headers { Headers([body.contentHeader]) }
+        }
+        let expected = """
+        curl \\
+          -X GET \\
+          -H 'Content-Length: 0' \\
+          "https://example.org/some/path"
+        """
+        XCTAssertEqual(EmptyBody().cURL(), expected)
+    }
+
+    func testStringBodyCurl() {
+        struct StringBody: Endpoint {
+            var method = HTTPMethod.post
+            var baseUrl = "https://example.org"
+            var path = "some/path"
+            var body = RequestBody.string(CodableString(string: "some string", encoding: .utf8))
+            var httpHeaderFields: Headers { Headers([body.contentHeader]) }
+        }
+        let expected = """
+        curl \\
+          -X POST \\
+          -H 'Content-Type: text/plain; charset=UTF-8' \\
+          --data-binary "some string" \\
+          "https://example.org/some/path"
+        """
+        XCTAssertEqual(StringBody().cURL(), expected)
+    }
+
+    func testDataBodyCurl() {
+        struct DataBody: Endpoint {
+            var method = HTTPMethod.put
+            var baseUrl = "https://example.org"
+            var path = "some/path"
+            var body = RequestBody.data("some data".data(using: .utf8) ?? Data())
+            var httpHeaderFields: Headers { Headers([body.contentHeader]) }
+
+        }
+        let expected = """
+        curl \\
+          -X PUT \\
+          -H 'Content-Type: application/octet-stream' \\
+          --data-binary "some data" \\
+          "https://example.org/some/path"
+        """
+        XCTAssertEqual(DataBody().cURL(), expected)
+    }
+
+    func testJsonBodyCurl() {
+        struct JsonBody: Endpoint {
+            var method = HTTPMethod.patch
+            var baseUrl = "https://example.org"
+            var path = "some/path"
+            var body = RequestBody.json(["some key": "some value"])
+            var httpHeaderFields: Headers { Headers([body.contentHeader]) }
+        }
+        let expected = """
+        curl \\
+          -X PATCH \\
+          -H 'Content-Type: application/json; charset=UTF-8' \\
+          --data-binary "{\\"some key\\":\\"some value\\"}" \\
+          "https://example.org/some/path"
+        """
+        XCTAssertEqual(JsonBody().cURL(), expected)
+    }
+
+    func testMultipartformDataBodyCurl() {
+        struct JsonBody: Endpoint {
+            var method = HTTPMethod.post
+            var baseUrl = "https://example.org"
+            var path = "some/path"
+            var body = RequestBody.multipartformData([MultipartParameter(data: Data(),
+                                                                         name: "someName",
+                                                                         fileName: "someFilename",
+                                                                         mimeType: "some/type")])
+            var httpHeaderFields: Headers { Headers([body.contentHeader]) }
+        }
+        let expected = """
+        curl \\
+          -X POST \\
+          -H 'Content-Type: multipart/form-data' \\
+          --form name=someName;filename=someFilename;data=@filename;type=some/type \\
+          "https://example.org/some/path"
+        """
+        XCTAssertEqual(JsonBody().cURL(), expected)
+    }
+}
